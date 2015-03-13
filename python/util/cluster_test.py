39d6ca8dc61b6a0a7defb1a95344ed264954b2cf
Alec Ten Harmsel <talec@umich.edu>
2015-03-13 17:38:02 -0400

    cluster_test.py refactoring

--- python/util/cluster_test.py
+++ python/util/cluster_test.py
@@ -2,11 +2,14 @@ import sys
 import argparse
 import subprocess
 import os
+from random import seed, randint
+
+seed()
 
 queue = 'staff'
 __version_hadoop__ = '2.5.0-cdh5.3.2'
 __version_spark__ = '1.2.0-cdh5.3.2'
-prefix = 'trozamon_testing'
+prefix = 'trozamon_testing' + str(randint(0, 100000))
 input_unstructured = '/'.join([prefix, 'input_unstructured'])
 input_structured = '/'.join([prefix, 'input_structured'])
 output_prefix = '/'.join([prefix, 'output'])
@@ -18,24 +21,27 @@ test suite after an upgrade can ensure that all expected functionality
 continues to work.  This script will generate and upload data to
 $HOME/trozamon_testing in HDFS."""
 
+def run_cmd(cmd):
+    print(' '.join(['Running:', cmd]))
+    return subprocess.call(cmd, shell=True)
+
 def hdfs_mkdir(dir):
-    if subprocess.call('hdfs dfs -test -d ' + dir, shell=True) != 0:
-        if subprocess.call('hdfs dfs -mkdir ' + dir, shell=True) != 0:
+    if run_cmd('hdfs dfs -test -d ' + dir) != 0:
+        if run_cmd('hdfs dfs -mkdir ' + dir) != 0:
             print('Failed to create ' + dir + ' in HDFS')
             return 1
     return 0
 
 def hdfs_put(local, remote):
-    if subprocess.call('hdfs dfs -test -f ' + remote, shell=True) != 0:
-        if subprocess.call('hdfs dfs -put ' + local + ' ' + remote,
-                shell=True) != 0:
+    if run_cmd('hdfs dfs -test -f ' + remote) != 0:
+        if run_cmd(' '.join(['hdfs dfs -put', local, remote])) != 0:
             print('Failed to upload ' + local + ' to ' + remote)
             return 1
     return 0
 
 def hdfs_rmdir(dir):
-    if subprocess.call('hdfs dfs -test -d ' + dir, shell=True) == 0:
-        return subprocess.call('hdfs dfs -rm -r ' + dir, shell=True)
+    if run_cmd('hdfs dfs -test -d ' + dir) == 0:
+        return run_cmd('hdfs dfs -rm -r ' + dir)
     return 0
 
 def test_hadoop_java():
@@ -51,10 +57,7 @@ def test_hadoop_java():
     if hdfs_rmdir(outdir) != 0:
         return 1
 
-    print('Testing Hadoop Java by running:')
-    print(cmd)
-
-    return subprocess.call(cmd, shell=True)
+    return run_cmd(cmd)
 
 def test_mrjob():
     cmd = ' '.join([
@@ -65,10 +68,7 @@ def test_mrjob():
         'hadoop'
         ])
 
-    print('Testing MRJob by running:')
-    print(cmd)
-
-    return subprocess.call(cmd, shell=True)
+    return run_cmd(cmd)
 
 def test_hadoop_streaming():
     outdir = '/'.join([output_prefix, 'hadoop_streaming'])
@@ -84,10 +84,7 @@ def test_hadoop_streaming():
     if hdfs_rmdir(outdir) != 0:
         return 1
 
-    print('Testing Hadoop Streaming by running:')
-    print(cmd)
-
-    return subprocess.call(cmd, shell=True)
+    return run_cmd(cmd)
 
 def test_pig():
     cmd = ' '.join([
@@ -96,10 +93,7 @@ def test_pig():
         '-f pig/cluster_test.pig'
         ])
 
-    print('Testing Pig by running:')
-    print(cmd)
-
-    return subprocess.call(cmd, shell=True)
+    return run_cmd(cmd)
 
 def test_spark():
     outdir = '/'.join([output_prefix, 'spark'])
@@ -116,10 +110,7 @@ def test_spark():
     if hdfs_rmdir(outdir) != 0:
         return 1
 
-    print('Testing Spark by running:')
-    print(cmd)
-
-    return subprocess.call(cmd, shell=True)
+    return run_cmd(cmd)
 
 def test_pyspark():
     cmd = ' '.join([
@@ -130,10 +121,7 @@ def test_pyspark():
         input_unstructured
         ])
 
-    print('Testing PySpark by running:')
-    print(cmd)
-
-    return subprocess.call(cmd, shell=True)
+    return run_cmd(cmd)
 
 def test_hive():
     cmd = ' '.join([
@@ -142,10 +130,7 @@ def test_hive():
         '-f hive/cluster_test.sql'
         ])
 
-    print('Testing Hive by running:')
-    print(cmd)
-
-    return subprocess.call(cmd, shell=True)
+    return run_cmd(cmd)
 
 def run():
     parser = argparse.ArgumentParser(description=big_description)
@@ -204,13 +189,12 @@ def run():
     if parsed_args.hive:
         tests.append(test_hive)
 
-    print('Running "mvn package -DskipTests"...')
-    if subprocess.call('mvn package -DskipTests', shell=True) != 0:
+    if run_cmd('mvn package -DskipTests') != 0:
         print('Maven build failed. Are you running this in the root ' +
                 'directory of the repo?')
         return 1
 
-    print('Creating directory structure...')
+    print('Uploading data...')
     err = hdfs_mkdir(prefix)
     if err != 0:
         return err
@@ -223,11 +207,9 @@ def run():
     err = hdfs_mkdir(output_prefix)
     if err != 0:
         return err
-    print('Uploading unstructured data...')
     err = hdfs_put('pom.xml', '/'.join([input_unstructured, 'pom.xml']))
     if err != 0:
         return err
-    print('Uploading structured data...')
     err = hdfs_put('structured.data', '/'.join([input_structured, 'data']))
     if err != 0:
         return err
